24ccca0c9db0e067228b35846689dbfd1b3280c4
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <ar6167423@gmail.com>
2025-03-22 12:31:56 +0500

    Update malvin.js

--- malvin.js
+++ malvin.js
@@ -1,41 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-
-// Import routes
-const qr = require('./qr');
-const code = require('./pair');
-
-// Prevent max listeners warning
+let server = require('./wasiqr'),
+    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Middleware setup
+app.use('/wasiqr', server);
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/wasipage.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Define routes
-app.use('/qr', qr);
-app.use('/code', code);
-app.use('/pair', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pair.html'));
-});
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`
-ALI XMD scanner online ✅
+Don't Forget To Give Star
 
-MADE BY ALI 
+ Server running on http://localhost:` + PORT)
+})
 
-Server running on http://localhost:${PORT}
-    `);
-});
-
-// Export the app instance
-module.exports = app;
-    
+module.exports = app
+/**
+    powered by wasi tech team 
+    join Whatsapp channel for more updates 
+    **/
